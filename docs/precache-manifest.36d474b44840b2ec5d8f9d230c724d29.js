d5fd205245e474ec614775d8efdda1d5b48d02dc
Noelia Luque <n.luque91@gmail.com>
2020-01-13 08:30:09 +0100

    run docs and installed react-router

---
+++ docs/precache-manifest.36d474b44840b2ec5d8f9d230c724d29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3524cafc0a0bc2f0f884776f2cebae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ad0df16b3364bd20b93",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "65a648d65e8e0e445485",
+    "url": "/static/js/2.616df7bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.616df7bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ad0df16b3364bd20b93",
+    "url": "/static/js/main.914a14f0.chunk.js"
+  },
+  {
+    "revision": "e8c51ed4ad10718cf5bb",
+    "url": "/static/js/runtime-main.eb6d63ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
