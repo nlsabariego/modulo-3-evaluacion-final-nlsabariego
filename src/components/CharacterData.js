331bea38a5e69c1c4c7e994db1bcc1851f5e93f3
Noelia Luque <n.luque91@gmail.com>
2020-01-13 17:50:43 +0100

    add styles

--- src/components/CharacterData.js
+++ src/components/CharacterData.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../stylesheets/CharacterData.scss";
 
 function CharacterData(props) {
   const { image, name, status, species, origin, episode } = props.character;
 
   return (
-    <div>
+    <div className='characterData'>
       <Link to='/'>
         <button className='form__btn'>Volver</button>
       </Link>
-      <div className='cards'>
-        <img src={image} alt={name} />
+      <div className='cardData'>
+        <img className='cardData__img' src={image} alt={name} />
         <div>
-          <p className='card__description'>Nombre: {name}</p>
-          <p className='card__description'>Estado: {status}</p>
-          <p className='card__description'>Especie: {species}</p>
-          <p className='card__description'>Origen: {origin.name}</p>
-          <p className='card__description'>Nº de capítulos: {episode.length}</p>
+          <p className='cardData__description'>Nombre: {name}</p>
+          <p className='cardData__description'>Estado: {status}</p>
+          <p className='cardData__description'>Especie: {species}</p>
+          <p className='cardData__description'>Origen: {origin.name}</p>
+          <p className='cardData__description'>Nº de capítulos: {episode.length}</p>
         </div>
       </div>
     </div>
