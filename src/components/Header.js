72755a6ebfa94a0e36d739448b9fab504231e0de
Noelia Luque <n.luque91@gmail.com>
2020-01-13 13:24:05 +0100

    install node-sass

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className='header'>
       <img src='https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png' alt='Rick and Morty' />
     </header>
   );
